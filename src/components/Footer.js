1fd5aa1cc9dfcf1448cc19ab75e65a58e9227412
Atanu_Mobeet <atanu.fat.mobeet@gmail.com>
2025-06-20 14:45:44 +0530

    Latest changes

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Little Lemon. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+    return (
+        <main>
+            <h1>Welcome to Little Lemon</h1>
+            <p>Your favorite Mediterranean Bistro!</p>
+        </main>
+    );
+};
+
+export default Main;
