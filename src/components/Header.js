cfc23c7be8335ff6e4e9adf3fa7c3be9295af11a
Atanu_Mobeet <atanu.fat.mobeet@gmail.com>
2025-06-20 17:55:02 +0530

    initial styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
+// components/Header.jsx
+import { useState } from "react";
 import Logo from "../assets/Logo.svg";
+import Hamburger from "../assets/hamburger.svg";
+import "../App.css"; // Make sure your styles are loaded
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <header>
-            <img src={Logo} alt="Little Lemon Logo" />
+            <div className="header-container">
+                <img src={Logo} alt="Little Lemon Logo" className="logo" />
+                <img
+                    src={Hamburger}
+                    alt="Menu"
+                    className="hamburger-icon"
+                    onClick={() => setIsMenuOpen((prev) => !prev)}
+                />
+
+                <nav className={`nav-menu ${isMenuOpen ? "open" : ""}`}>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/order">Order Online</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
